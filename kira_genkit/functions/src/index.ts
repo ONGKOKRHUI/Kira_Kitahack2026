4c04a5725f27febd916074a68ae2cdf5778c5e9a
Excelyynx <excelyynxl@gmail.com>
2026-02-03 16:15:32 +0800

    implement extractInvoice() flow

--- kira_genkit/functions/src/index.ts
+++ kira_genkit/functions/src/index.ts
@@ -1,44 +1,13 @@
 import 'dotenv/config'; // additional installation
 import {genkit, z} from "genkit";
-import {googleAI} from "@genkit-ai/google-genai";
-
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import { googleAI } from '@genkit-ai/google-genai';
+import path from "path";
+import { readFile } from 'node:fs/promises'; 
 
 const ai = genkit({
-  plugins: [
-    googleAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY})
-  ],
+    plugins: [
+        googleAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY })
+    ],
 });
 
 // define item schema
@@ -46,21 +15,28 @@ const ItemSchema = ai.defineSchema(
     'ItemSchema',
     z.object({
         // think about what attributes does an ItemSchema have, how are each described
-        name: z.string().describe("The name of item purchase in this invoice (eg: petrol, solar panel, diesal, biodegradable container)."),
+        name: z.string().describe("The name of for this line item (eg: petrol, solar panel, diesal, biodegradable container)."),
         supplier: z.string().describe("The supplier of this invoice."),
         quantity: z.number().describe("The amount purchased for this item."),
-        unit: z.string().describe("The unit (eg: single, kg, litres) purchased for this item."),
-        price: z.string().describe("The price of item purchased."),
-        dateOfPurchase: z.date().describe("The date of this invoice issued.")
+        unit: z.string().describe("The unit (eg: single, kg, litres) purchased for this line item."),
+        price: z.string().describe("The total price for this line item."),
+        currency: z.string().default("MYR"),
+        isGitaEligible: z.boolean().describe("Whether this item qualifies for GITA (Green Investment Tax Incentive)"),
+        dateOfPurchase: z.boolean().describe("The date of this invoice issued in YYYY-MM-DD format.")
     })
 );
+
+// define array of items as a single invoie can have multiple item
+const InvoiceResponseSchema = z.object({
+    items: z.array(ItemSchema),
+    totalAmount: z.number(),
+    invoiceNumber: z.string()
+})
 
 // define gitaEntry schema
 const GITAEntrySchema = ai.defineSchema(
     'GITAEntrySchema',
     z.object({
-        // think about what attributes does an GITAEntrySchema have, how are each described
-        isEligible: z.boolean().describe("Is this item eligible for Green Investment Tax Allowance (GITA) Malaysia?"),
         tier: z.number().describe("The tier of this GITA entry if eligible for Green Investment Tax Allowance (GITA) Malaysia."),
         sector: z.string().describe("The sector (eg: energy efficiency, renewable energy system, waste, water) of this GITA entry."),
         technology: z.string().describe("The technology (eg: Transformer, Energy Efficient Appliances, Chiller, etc) of this GITA entry."),
@@ -70,10 +46,11 @@ const GITAEntrySchema = ai.defineSchema(
 
 
 // define carbonEntry schema
+// scope 1: 𝐶𝑂2𝑒 𝑒𝑚𝑖𝑠𝑠𝑖𝑜𝑛𝑠 = ∑𝐴𝑐𝑡𝑖𝑣𝑖𝑡𝑦 𝐷𝑎𝑡𝑎 × 𝐸𝑚𝑖𝑠𝑠𝑖𝑜𝑛 𝐹𝑎𝑐𝑡𝑜𝑟 × 𝐺𝑊P, 
+// scope 2: 𝐶𝑂2𝑒 𝑒𝑚𝑖𝑠𝑠𝑖𝑜𝑛𝑠 = 𝐸𝑙𝑒𝑐𝑡𝑟𝑖𝑐𝑖𝑡𝑦 𝑝𝑢𝑟𝑐ℎ𝑎𝑠𝑒𝑑 × 𝐺𝐸F
 const CarbonEntrySchema = ai.defineSchema(
     'CarbonEntrySchema',
     z.object({
-        // think about what attributes does an CarbonEntrySchema have, how are each described
         scope: z.number().describe("Scope (eg: 1,2,3) according to Greenhouse Gas Protocol (GHG) of this carbon entry."),
         activityData: z.number().describe("The activity data of carbon emission of this carbon entry."),
         emissionFactor: z.number().describe("The activity data emission factor of carbon emission of this carbon entry if this entry is of scope 1."),
@@ -84,7 +61,49 @@ const CarbonEntrySchema = ai.defineSchema(
 );
 
 // define flow for extractInvoice()
+// output an array on item since a single  invoice can have multiple items
+export const extractInvoice = ai.defineFlow(
+    {
+        name: 'extractInvoice', 
+        inputSchema: z.object({
+            file: z.string().describe("Local path to the invoice file"),
+        }),
+        outputSchema: InvoiceResponseSchema 
+    },
+    async (input) => {
+        const { dataUrl } = await ai.run('load-invoice-file', async () => {
+            const filePath = path.resolve(input.file);
+            const fileExtension = path.extname(filePath).toLowerCase();
+            
+            let mimeType = fileExtension === '.pdf' ? 'application/pdf' : `image/${fileExtension.replace('.', '')}`;
+            const b64Data = await readFile(filePath, { encoding: 'base64' });
+            return { dataUrl: `data:${mimeType};base64,${b64Data}` };
+        });
 
-// define flow for determineIsGitaEntry()
+        const { output } = await ai.generate({
+            model: googleAI.model('gemini-2.5-flash'),
+            prompt: [
+                { media: { url: dataUrl } },
+                { text: "Extract all line items and total amounts from this invoice. Identify if assets are eligible for Green Investment Tax Incentive (GITA)." },
+            ],
+            output: {
+                schema: InvoiceResponseSchema,
+            }
+        });
 
-// define flow for determineIsCarbonEntry()
+        // error handling
+        if (!output) {
+            throw new Error('Extraction failed.');
+        }
+
+        return output;
+    }
+);
+
+// define flow for processInvoiceItems()
+
+
+// define flow for convertToGitaEntry()
+
+
+// define flow for convertToCarbonEntry()
